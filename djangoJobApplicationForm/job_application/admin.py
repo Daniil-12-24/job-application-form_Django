af09bb01e68c220888e03320f05e2ee0475cea87
danik <smokequitz@gmail.com>
2023-06-07 10:31:49 +0100

    Finished front page, added navigation bar, a few links, added an admin interface, customized admin interface.

--- djangoJobApplicationForm/job_application/admin.py
+++ djangoJobApplicationForm/job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email') # Creates a current table with given fields
+    search_fields = ('first_name', 'last_name', 'email') # Creates a search field
+    list_filter = ('date', 'occupation') # Creates a filter table with given fields
+    ordering = ('first_name',) # Sets the names in the alphabet order
+    readonly_fields = ('email', 'occupation',) # Makes a readonly field for given fields
+
+
+admin.site.register(Form, FormAdmin)
